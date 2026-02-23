b623b9e627607160514073de6a07e33a7ccf4f09
ZayMinKhant <zayminkhant.zay2@gmail.com>
2026-02-23 17:11:40 +0700

    fix: import State from the correct module in create-form component

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -9,9 +9,8 @@ import {
   UserCircleIcon,
 } from "@heroicons/react/24/outline";
 import { Button } from "@/app/ui/button";
-import { createInvoice } from "@/app/lib/actions";
+import { createInvoice, State } from "@/app/lib/actions";
 import { useActionState } from "react";
-import { State } from "postgres";
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
   const initialState: State = { message: null, errors: {} };
